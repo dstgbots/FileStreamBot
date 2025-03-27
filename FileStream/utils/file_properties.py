ad402a828b767d635e926dbc5f0aec674b8d2326
Naveen Kumar <70622189+Ns-AnoNymouS@users.noreply.github.com>
2025-03-27 22:28:22 +0530

    fixed local variable refereenced before error

--- FileStream/utils/file_properties.py
+++ FileStream/utils/file_properties.py
@@ -102,6 +102,7 @@ def get_media_file_size(m):
 
 
 def get_name(media_msg: Message | FileId) -> str:
+    file_name = None
     if isinstance(media_msg, Message):
         media = get_media_from_message(media_msg)
         file_name = getattr(media, "file_name", "")
