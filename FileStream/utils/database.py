edcd5208a1e2d8ba92bb4bc7f03c04d2b32fe3f2
Naveen Kumar <70622189+Ns-AnoNymouS@users.noreply.github.com>
2025-03-27 15:50:35 +0530

    .

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -68,6 +68,7 @@ class Database:
         
 # ---------------------[ ADD FILE TO DB ]---------------------#
     async def add_file(self, file_info):
+        print(file_info)
         file_info["time"] = time.time()
         fetch_old = await self.get_file_by_fileuniqueid(file_info["user_id"], file_info["file_unique_id"])
         if fetch_old:
